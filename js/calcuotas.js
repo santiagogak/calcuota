48acac5c37b52867755b3e6acad2068ec434df49
Santiago Adolfo <sgutierrezakel@gmail.com>
2024-11-06 01:16:19 -0300

    se agrega algo de css y formato de monedas

--- js/calcuotas.js
+++ js/calcuotas.js
@@ -43,6 +43,9 @@ const pagoMensual = (cap, int, cuo) => {
     }
 };
 
+//Funcion para dar formato de moneda al número
+const formatoMoneda = (monto) => monto.toLocaleString('es-CL', {style: 'currency', currency: 'CLP', maximumFractionDigits: 2 });
+
 //Función para calcular todos los pagos mensuales mes a mes y el interés que se paga en cada uno, así como el total de interes pagado
 function detallePagos (cap, int, cuo) {
     const intN = int / 100;
@@ -54,16 +57,16 @@ function detallePagos (cap, int, cuo) {
         let totalInteres = 0;
         for (let i = 1; i <= cuotas; i++) {
             const capMes = restoCap * (1+intN);
-            strPagos = strPagos + `Pago ${i}: ${restoCap.toFixed(2)} - Interés: ${(restoCap*intN).toFixed(2)}\n`;
+            strPagos = strPagos + `Pago ${i}: ${formatoMoneda(restoCap)} - Interés: ${formatoMoneda(restoCap*intN)}\n`;
             totalInteres += restoCap*intN;
             restoCap = capMes - pagoMes;
         }
-        strPagos = strPagos + `\nTotal Intereses: ${totalInteres.toFixed(2)}`;
+        strPagos = strPagos + `\nTotal Intereses: ${formatoMoneda(totalInteres)}`;
     } else {
-        strPagos = strPagos + `Pago: ${cap} - Interés: 0`;
+        strPagos = strPagos + `Pago: ${formatoMoneda(cap)} - Interés: 0`;
     }
     return strPagos;
 }
 
-alert(`Pago Mensual: ${pagoMensual(capital,interes,cuotas).toFixed(2)}`)
+alert(`Pago Mensual: ${formatoMoneda(pagoMensual(capital,interes,cuotas))}`)
 alert(detallePagos(capital,interes,cuotas))
